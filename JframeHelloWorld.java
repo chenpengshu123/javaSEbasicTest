8f7772697980a30e64f1b2396c173b56587a1831
cps <1974374857@qq.com>
2019-08-02 17:52:03 +0800

    第一次提交

---
+++ JframeHelloWorld.java
@@ -0,0 +1,50 @@
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class JframeHelloWorld{
+    public static void main(String[] args){
+		int a = 1;
+		a=++a;
+		a=a++;
+		a=a++ + ++a;
+		System.out.println(a-- + ++a + a++ + a);
+		System.out.println(a);
+	}
+}
+	    /*JFrame frame = new JFrame("???????Jframe");
+		JPanel panel = new JPanel();
+		JLabel label = new JLabel("helloworld");
+		panel.add(label);
+		frame.setContentPane(panel);
+		frame.setSize(300,200);
+		frame.setVisible(true);*/
+		/*Random random = new Random();
+		char c =(char)('A'+random.nextInt(26));
+		System.out.print(c);*/
+		/*int a = random.nextInt(26);
+		int b=a+65;
+		char c=(char)b;
+		System.out.print(c);*/
+		/*Scanner scanner = new Scanner(System.in);
+		System.out.print("??????§³?????");	
+		int n=scanner.nextInt();
+		int a=(int)Math.pow(n,2);
+		System.out.print(a);*/
+   /* public static int getSum(int a,int b){
+			int result = a+b;
+			return result;
+			
+    }
+    public static int getSub(int a,int b){
+			int result = a-b;
+			return result;
+    }
+    public static int getMul(int a,int b){
+			int result = a*b;
+			return result;
+    }
+     public static int getDiv(int a,int b){
+			int result = a/b;  
+}*/
+
